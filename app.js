a97efa04a5d086fcdcd69f28f91af77ce6fd3a77
Ryan Mok <ryan-mok@users.noreply.github.com>
2020-11-03 16:07:41 -0500

    Add test /news endpoint that returns single result

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const http = require('http');
+const url = require('url');
 const express = require("express");
+const { request } = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 
@@ -27,6 +30,33 @@ app.get("/2", (req, res, next) => {
 		imageManipulated: false,
 		urlSuspicious: false,
 		headlineSuspicious: false
+	});
+});
+
+app.get("/news", (req, res, next) => {
+	let q = req.query.q;
+	var requestUrl = url.parse(url.format({
+		protocol: 'http',
+		hostname: 'newsapi.org',
+		pathname: '/v2/everything',
+		query: {
+			q: q,
+			from: new Date().toISOString().split('T')[0],
+			sortBy: 'relevancy',
+			pageSize: '1',
+			apiKey: process.env.NEWS_API_KEY
+		}
+	}));
+	http.get(requestUrl, (resp) => {
+		let data = '';
+		resp.on('data', (chunk) => {
+			data += chunk;
+		});
+		resp.on('end', () => {
+			res.json(JSON.parse(data));
+		});
+	}).on("error", (err) => {
+		console.log("Error: " + err.message);
 	});
 });
 
