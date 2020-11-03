7402e636371077912084908beb1a08a6d70d7d51
Ryan Mok <ryan-mok@users.noreply.github.com>
2020-11-03 15:29:27 -0500

    Add echo and two samples

--- app.js
+++ app.js
@@ -2,8 +2,36 @@ const express = require("express");
 const app = express();
 var port = process.env.PORT || 3000;
 
-app.get("/test", (req, res, next) => {
-	res.json({hello: "World!"});
+app.get("/1", (req, res, next) => {
+	res.json({
+		topicCoverage: 1,
+		publisherQuality: 1,
+		publisherBias: "Right",
+		satire: true,
+		evidenceCited: true,
+		authorVerified: true,
+		imageManipulated: true,
+		urlSuspicious: true,
+		headlineSuspicious: true
+	});
+});
+
+app.get("/2", (req, res, next) => {
+	res.json({
+		topicCoverage: 5,
+		publisherQuality: 5,
+		publisherBias: "Left",
+		satire: false,
+		evidenceCited: false,
+		authorVerified: false,
+		imageManipulated: false,
+		urlSuspicious: false,
+		headlineSuspicious: false
+	});
+});
+
+app.get("/echo", (req, res, next) => {
+	res.json({headers: req.headers, body: req.body});
 });
 
 app.use(function(req, res) {
