af645b7ad9d1d309b356a53f90831df97f640a8c
Ryan Mok <ryan-mok@users.noreply.github.com>
2021-03-02 00:17:57 -0500

    Make date parsing more robust

--- document_parser.js
+++ document_parser.js
@@ -21,17 +21,20 @@ module.exports = {
 
 		let json_ld_date_modified, json_ld_date_published
 		try {
-			let json_ld = JSON.parse($('script[type="application/ld+json"]')[0].children[0].data)
+			let json_ld_html = $('script[type="application/ld+json"]')[0] || $('script[type="application/ld+json"]')
+			let json_ld = JSON.parse(json_ld_html.text)
 			json_ld_date_modified = json_ld.dateModified
 			json_ld_date_published = json_ld.datePublished
 		} catch (e) {}
 
+		let article_modified_time = $('meta[property="article:modified_time"]').attr('content')
+		let article_published_time = $('meta[property="article:published_time"]').attr('content')
 		let article_published = $('meta[name="article.published"]').attr('content')
 		let bt_pubDate = $('meta[property="bt:pubDate"]').attr('content')
 		let dc_date_issued = $('meta[name="DC.date.issued"]').attr('content')
 		let pubdate = $('meta[name="pubdate"]').attr('content')
 
-		let date = json_ld_date_modified || json_ld_date_published || article_published || bt_pubDate || dc_date_issued || pubdate
+		let date = json_ld_date_modified || json_ld_date_published || article_modified_time || article_published_time || article_published || bt_pubDate || dc_date_issued || pubdate
 		let image = $('meta[name="twitter:image"]').attr('content') || $('meta[name="twitter:image:src"]').attr('content')
 
 		return [date, image]
